10fbeee11acc60824ca89bfe95ca173701140a16
Niall Riddell <Niall.Riddell@pega.com>
2024-10-25 13:54:21 +0100

    added nonce

--- src/components/Sl_DXExtensions_StarRating/StarRating.tsx
+++ src/components/Sl_DXExtensions_StarRating/StarRating.tsx
@@ -1,5 +1,18 @@
-import { useCallback, useEffect, useState, forwardRef, MouseEvent } from 'react';
-import type { KeyboardEvent, FunctionComponent, PropsWithoutRef, Ref, TouchEvent } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useState,
+  forwardRef,
+  MouseEvent
+} from 'react';
+import type {
+  KeyboardEvent,
+  FunctionComponent,
+  PropsWithoutRef,
+  Ref,
+  TouchEvent
+} from 'react';
+
 import {
   Flex,
   Icon,
@@ -11,12 +24,14 @@ import {
   cap
 } from '@pega/cosmos-react-core';
 import type { ForwardProps } from '@pega/cosmos-react-core';
-import type { StarRatingProps } from './StarRating.types';
-import type { PConnProps } from './PConnProps';
-
 import * as star from '@pega/cosmos-react-core/lib/components/Icon/icons/star.icon';
 import * as starSolid from '@pega/cosmos-react-core/lib/components/Icon/icons/star-solid.icon';
+
+import type { PConnProps } from './PConnProps';
+import './create-nonce';
+
 import { StyledStarWrapper, StyledStarRatingMetaInfo } from './styles';
+import type { StarRatingProps } from './StarRating.types';
 
 registerIcon(star, starSolid);
 
@@ -62,17 +77,17 @@ const SlDXExtensionsStarRating: FunctionComponent<
     getPConnect = undefined
   } = props;
 
-    const starRatingRef = useConsolidatedRef<HTMLDivElement>(ref);
+  const starRatingRef = useConsolidatedRef<HTMLDivElement>(ref);
 
-    const [inHover, setInHovering] = useState(false);
-    const [currentHoverValue, setCurrentHoverValue] = useState<number>(value);
-    const [currentValue, setCurrentValue] = useState<number>(value);
-    const [metaInfoUpdated, setMetaInfoUpdated] = useState(metaInfo);
+  const [inHover, setInHovering] = useState(false);
+  const [currentHoverValue, setCurrentHoverValue] = useState<number>(value);
+  const [currentValue, setCurrentValue] = useState<number>(value);
+  const [metaInfoUpdated, setMetaInfoUpdated] = useState(metaInfo);
 
-    const setValue = useCallback(
-      (newValue: number, ignoreChange?: boolean) => {
-        if (disabled || readOnly) return;
-        const normalizedValue = Math.min(Math.max(newValue, min), max);
+  const setValue = useCallback(
+    (newValue: number, ignoreChange?: boolean) => {
+      if (disabled || readOnly) return;
+      const normalizedValue = Math.min(Math.max(newValue, min), max);
 
       setCurrentHoverValue(normalizedValue);
       setCurrentValue(normalizedValue);
@@ -83,187 +98,195 @@ const SlDXExtensionsStarRating: FunctionComponent<
     [starRatingRef, disabled, readOnly, onChange, min, max, getPConnect]
   );
 
-    const { start, end, rtl } = useDirection();
+  const { start, end, rtl } = useDirection();
 
-    const onKeyDown = useCallback(
-      (e: KeyboardEvent<HTMLDivElement>) => {
-        if (readOnly || disabled) return;
-        if (
-          [
-            'ArrowDown',
-            'ArrowUp',
-            'ArrowLeft',
-            'ArrowRight',
-            'PageUp',
-            'PageDown',
-            'Home',
-            'End'
-          ].includes(e.key)
-        )
-          e.preventDefault();
-
-        switch (e.key) {
-          case 'ArrowDown':
-          case `Arrow${cap(start)}`:
-            setValue(currentValue - 1);
-            break;
-          case 'ArrowUp':
-          case `Arrow${cap(end)}`:
-            setValue(currentValue + 1);
-            break;
-          case 'PageUp':
-            setValue(currentValue + 1);
-            break;
-          case 'PageDown':
-            setValue(currentValue - 1);
-            break;
-          case 'Home':
-            setValue(min);
-            break;
-          case 'End':
-            setValue(max);
-            break;
-          default:
-        }
-      },
-      [setValue, currentValue, start, end, disabled, max, min, readOnly]
-    );
-
-    useEffect(() => {
-      setValue(value, true);
-    }, [setValue, value]);
-
-    useEffect(() => {
-      if (autoFocus) starRatingRef.current?.focus();
-    }, [autoFocus, starRatingRef]);
-
-    // *** Thanks to shoelace
-    // https://github.com/shoelace-style/shoelace/blob/next/src/components/rating/rating.component.ts
-    const roundToPrecision = (numberToRound: number, precision = 1): number => {
-      const multiplier = 1 / precision;
-      return Math.ceil(numberToRound * multiplier) / multiplier;
-    };
-
-    const getValueFromXCoordinate = (coordinate: number): number => {
-      const { left, right, width } = starRatingRef.current?.getBoundingClientRect() as DOMRect;
-      const valueFromX = rtl
-        ? roundToPrecision(((right - coordinate) / width) * max)
-        : roundToPrecision(((coordinate - left) / width) * max);
-
-      return Math.max(Math.min(valueFromX, max), min);
-    };
-
-    const getValueFromEventPosition = (event: MouseEvent | TouchEvent): number => {
-      const clientX = 'touches' in event ? event.touches[0].clientX : event.clientX;
-      return getValueFromXCoordinate(clientX);
-    };
-    //
-
-    const onClick = (e: MouseEvent) => {
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent<HTMLDivElement>) => {
       if (readOnly || disabled) return;
+      if (
+        [
+          'ArrowDown',
+          'ArrowUp',
+          'ArrowLeft',
+          'ArrowRight',
+          'PageUp',
+          'PageDown',
+          'Home',
+          'End'
+        ].includes(e.key)
+      )
+        e.preventDefault();
 
-      const newValue = getValueFromEventPosition(e);
-
-      if (newValue === currentValue) {
-        setValue(0);
-        setInHovering(false);
-        return;
+      switch (e.key) {
+        case 'ArrowDown':
+        case `Arrow${cap(start)}`:
+          setValue(currentValue - 1);
+          break;
+        case 'ArrowUp':
+        case `Arrow${cap(end)}`:
+          setValue(currentValue + 1);
+          break;
+        case 'PageUp':
+          setValue(currentValue + 1);
+          break;
+        case 'PageDown':
+          setValue(currentValue - 1);
+          break;
+        case 'Home':
+          setValue(min);
+          break;
+        case 'End':
+          setValue(max);
+          break;
+        default:
       }
-      setValue(newValue);
-    };
+    },
+    [setValue, currentValue, start, end, disabled, max, min, readOnly]
+  );
 
-    const onMouseEnter = (e: MouseEvent) => {
-      if (readOnly || disabled) return;
+  useEffect(() => {
+    setValue(value, true);
+  }, [setValue, value]);
 
-      setInHovering(true);
-      setCurrentHoverValue(getValueFromEventPosition(e));
-    };
+  useEffect(() => {
+    if (autoFocus) starRatingRef.current?.focus();
+  }, [autoFocus, starRatingRef]);
 
-    const onMouseMove = (e: MouseEvent) => {
-      if (readOnly || disabled) return;
+  // *** Thanks to shoelace
+  // https://github.com/shoelace-style/shoelace/blob/next/src/components/rating/rating.component.ts
+  const roundToPrecision = (numberToRound: number, precision = 1): number => {
+    const multiplier = 1 / precision;
+    return Math.ceil(numberToRound * multiplier) / multiplier;
+  };
 
-      if (inHover) setCurrentHoverValue(getValueFromEventPosition(e));
-    };
+  const getValueFromXCoordinate = (coordinate: number): number => {
+    const { left, right, width } =
+      starRatingRef.current?.getBoundingClientRect() as DOMRect;
+    const valueFromX = rtl
+      ? roundToPrecision(((right - coordinate) / width) * max)
+      : roundToPrecision(((coordinate - left) / width) * max);
 
-    const onMouseLeave = () => {
-      if (readOnly || disabled) return;
+    return Math.max(Math.min(valueFromX, max), min);
+  };
 
+  const getValueFromEventPosition = (
+    event: MouseEvent | TouchEvent
+  ): number => {
+    const clientX =
+      'touches' in event ? event.touches[0].clientX : event.clientX;
+    return getValueFromXCoordinate(clientX);
+  };
+  //
+
+  const onClick = (e: MouseEvent) => {
+    if (readOnly || disabled) return;
+
+    const newValue = getValueFromEventPosition(e);
+
+    if (newValue === currentValue) {
+      setValue(0);
       setInHovering(false);
-      setCurrentHoverValue(currentValue);
-    };
+      return;
+    }
+    setValue(newValue);
+  };
 
-    const onTouchStart = (e: TouchEvent) => {
-      if (readOnly || disabled) return;
+  const onMouseEnter = (e: MouseEvent) => {
+    if (readOnly || disabled) return;
 
-      setInHovering(true);
-      setCurrentHoverValue(getValueFromEventPosition(e));
+    setInHovering(true);
+    setCurrentHoverValue(getValueFromEventPosition(e));
+  };
 
-      // Prevent scrolling when touch is initiated
-      e.preventDefault();
-    };
+  const onMouseMove = (e: MouseEvent) => {
+    if (readOnly || disabled) return;
 
-    const onTouchMove = (e: TouchEvent) => {
-      if (readOnly || disabled) return;
+    if (inHover) setCurrentHoverValue(getValueFromEventPosition(e));
+  };
 
-      setCurrentHoverValue(getValueFromEventPosition(e));
-    };
+  const onMouseLeave = () => {
+    if (readOnly || disabled) return;
 
-    const onTouchEnd = () => {
-      if (readOnly || disabled) return;
+    setInHovering(false);
+    setCurrentHoverValue(currentValue);
+  };
 
-      setInHovering(false);
-    };
+  const onTouchStart = (e: TouchEvent) => {
+    if (readOnly || disabled) return;
 
-    return (
-      <Flex container={{ direction: 'row' }}>
-        <Flex
-          id={id}
-          as={StyledStarWrapper}
-          ref={starRatingRef}
-          container={{ direction: 'row', alignItems: 'start' }}
-          role='slider'
-          aria-label={label}
-          aria-disabled={disabled ? 'true' : 'false'}
-          aria-readonly={readOnly ? 'true' : 'false'}
-          aria-valuenow={value}
-          aria-valuemin={0}
-          aria-valuemax={max}
-          tabIndex={disabled ? '-1' : '0'}
-          disabled={disabled}
-          readOnly={readOnly}
-          onClick={onClick}
-          onMouseEnter={onMouseEnter}
-          onMouseMove={onMouseMove}
-          onTouchStart={onTouchStart}
-          onTouchMove={onTouchMove}
-          onTouchEnd={onTouchEnd}
-          onKeyDown={onKeyDown}
-          onMouseLeave={onMouseLeave}
-          style={{ display: 'inline-flex' }}
-          data-test-id={testId}
-          required={required}
-        >
-          <Flex item={{ shrink: 0 }}>
-            {Array.from({ length: max }, (_, i) => {
-              const rating = i + 1;
-              const isSolid = rating <= currentHoverValue;
-              return (
-                <Icon
-                  className={`star ${disabled ? 'disabled' : ''} ${readOnly ? 'readonly' : ''}`}
-                  key={`Icon-${i}`}
-                  name={isSolid ? 'star-solid' : 'star'}
-                />
-              );
-            })}
-          </Flex>
+    setInHovering(true);
+    setCurrentHoverValue(getValueFromEventPosition(e));
+
+    // Prevent scrolling when touch is initiated
+    e.preventDefault();
+  };
+
+  const onTouchMove = (e: TouchEvent) => {
+    if (readOnly || disabled) return;
+
+    setCurrentHoverValue(getValueFromEventPosition(e));
+  };
+
+  const onTouchEnd = () => {
+    if (readOnly || disabled) return;
+
+    setInHovering(false);
+  };
+
+  return (
+    <Flex container={{ direction: 'row' }}>
+      <Flex
+        id={id}
+        as={StyledStarWrapper}
+        ref={starRatingRef}
+        container={{ direction: 'row', alignItems: 'start' }}
+        role='slider'
+        aria-label={label}
+        aria-disabled={disabled ? 'true' : 'false'}
+        aria-readonly={readOnly ? 'true' : 'false'}
+        aria-valuenow={value}
+        aria-valuemin={0}
+        aria-valuemax={max}
+        tabIndex={disabled ? '-1' : '0'}
+        disabled={disabled}
+        readOnly={readOnly}
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseMove={onMouseMove}
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
+        onKeyDown={onKeyDown}
+        onMouseLeave={onMouseLeave}
+        style={{ display: 'inline-flex' }}
+        data-test-id={testId}
+        required={required}
+      >
+        <Flex item={{ shrink: 0 }}>
+          {Array.from({ length: max }, (_, i) => {
+            const rating = i + 1;
+            const isSolid = rating <= currentHoverValue;
+            return (
+              <Icon
+                className={`star ${disabled ? 'disabled' : ''} ${
+                  readOnly ? 'readonly' : ''
+                }`}
+                key={`Icon-${i}`}
+                name={isSolid ? 'star-solid' : 'star'}
+              />
+            );
+          })}
         </Flex>
-        {metaInfoUpdated && (
-          <Flex item={{ shrink: 0 }}>
-            <StyledStarRatingMetaInfo>({metaInfoUpdated})</StyledStarRatingMetaInfo>
-          </Flex>
-        )}
       </Flex>
-    );
-  });
+      {metaInfoUpdated && (
+        <Flex item={{ shrink: 0 }}>
+          <StyledStarRatingMetaInfo>
+            ({metaInfoUpdated})
+          </StyledStarRatingMetaInfo>
+        </Flex>
+      )}
+    </Flex>
+  );
+});
 
 export default withConfiguration(SlDXExtensionsStarRating);
