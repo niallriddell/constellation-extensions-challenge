e0f23d8f3ca4a1319edaf0807d00c4892322f138
Niall Riddell <Niall.Riddell@pega.com>
2024-11-06 08:27:25 +0000

    removed unnecessary type declaration

--- src/components/Sl_DXExtensions_StarRatingWidget/index.tsx
+++ src/components/Sl_DXExtensions_StarRatingWidget/index.tsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import { Table, withConfiguration } from '@pega/cosmos-react-core';
 import type { Payload } from '@pega/pcore-pconnect-typedefs/data-view/types';
 import type { Parameters } from '@pega/pcore-pconnect-typedefs/datapage/types';
-import type { TableProps } from '@pega/cosmos-react-core/lib/components/Table/Table';
 
 import type { PConnFieldProps } from './PConnProps';
 import './create-nonce';
